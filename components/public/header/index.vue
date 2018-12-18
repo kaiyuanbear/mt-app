a37ebd31b1abc44f3c00e325bb9fba0506d82bde
cdluzhichao <cdluzhichao@jd.com>
2018-12-18 08:26:28 +0800

    feat 1. topbar和searchbar样式调整

--- components/public/header/index.vue
+++ components/public/header/index.vue
@@ -1,24 +1,28 @@
 <template>
-  <el-row 
-    :gutter="0" 
-    class="m-header">
-    <el-col :span="4"><geo/></el-col>
-    <el-col :span="6"><user/></el-col>
-    <el-col :span="14"><nav-bar/></el-col>
-  </el-row>
+  <section class="m-header">
+    <el-row>
+      <el-col>
+        <top-bar/>
+      </el-col>
+    </el-row>
+    <el-row>
+      <el-col>
+        <search-bar/>
+      </el-col>
+    </el-row>
+  </section>
 </template>
 <script>
-import Geo from './geo'
-import User from './user'
-import NavBar from './nav'
+import TopBar from './topbar'
+import SearchBar from './searchbar'
+
 export default {
   components: {
-    Geo,
-    User,
-    NavBar
+    TopBar,
+    SearchBar
   }
 }
 </script>
 <style lang="scss">
-  @import "@/assets/css/public/header.scss";
+@import '@/assets/css/public/header.scss';
 </style>
---
+++ components/public/header/searchbar.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="search-panel">
+    <el-row>
+      <el-col
+        :span="3"
+        class="left"
+      >
+        <img
+          alt="美团"
+          src="//s0.meituan.net/bs/fe-web-meituan/e5eeaef/img/logo.png"
+        >
+      </el-col>
+      <el-col
+        :span="12"
+        class="center"
+      >
+        <div class="wrapper">
+          <el-input placeholder="搜索商家或地点"/>
+          <button class="el-button el-button-primary">
+            <i class="el-icon-search"/>
+          </button>
+          <dl class="hotPlact">
+            <dt>热门搜索</dt>
+            <dd>火锅</dd>
+            <dd>火锅</dd>
+            <dd>火锅</dd>
+          </dl>
+          <dl class="searchList">
+            <dd>火锅</dd>
+            <dd>火锅</dd>
+            <dd>火锅</dd>
+          </dl>
+        </div>
+        <p class="suggest">
+          <a href="void(0)">故宫博物院</a>
+          <a href="void(0)">故宫博物院</a>
+          <a href="void(0)">故宫博物院</a>
+        </p>
+      </el-col>
+      <el-col
+        :span="6"
+        class="right"
+      >
+        <ul class="security">
+          <li>
+            <i class="refund">
+              <p class="txt">随时退</p>
+            </i>
+          </li>
+          <li>
+            <i class="single">
+              <p class="txt">不满意免单</p>
+            </i>
+          </li>
+          <li>
+            <i class="overdue">
+              <p class="txt">过期退</p>
+            </i>
+          </li>
+        </ul>
+      </el-col>
+    </el-row>
+  </div>
+</template>
